da1079dbc7fb549581cbb36c66a531a49a1a5db6
JungHyeokAhn <yehsi2000@gmail.com>
2023-02-19 22:13:21 +0900

    chore : fixed minor bugs
    changed include order to be safe
    reloaded buggy BatteryHUD

--- Source/BatteryCollector/Pickup.cpp
+++ Source/BatteryCollector/Pickup.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/StaticMeshComponent.h"
 #include "Pickup.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 APickup::APickup()
